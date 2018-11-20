f9a30157a781f4f3a6e06ca1a0efa0e650240888
EbichanOO <takanori.sao@icloud.com>
2018-11-20 16:34:59 +0900

    add main flow

--- dataEdit.py
+++ dataEdit.py
@@ -61,6 +61,41 @@ class DataSet:
         with open(savePath+dictName, 'rb') as f:
             return pkl.load(f)
 
-DD = DataSet()
-DD.MakeDict()
-#print(DD.readDict())
+    def readDataset(self):
+        endl = 0
+        dirList = os.listdir(self.filePath)
+        textFiles = []
+
+        for dump in dirList:
+            if endl == 3:
+                break
+            if(re.search('.txt',dump)):
+                textFiles.append(self.filePath + dump)
+        if(len(textFiles)==0):
+            print("not Found text files.")
+        else:
+            import MeCab
+            dictWords = []
+            print("Found {} text files.".format(len(textFiles)))
+            
+            tasknum = 0
+            spsp = 0
+            for dump in textFiles:
+                with open(dump, 'r', encoding='utf-8') as f:
+                    temp = f.read().split('\n')
+                
+                i = 0
+                for tmp in temp:
+                    if(len(tmp)!=0):
+                        if(tmp[0]=="F" or tmp[0]=="M" or tmp[0]=="%"): #1返答1行に分離
+                            i += 1
+                            sent = []
+                            if( len(tmp.split("：")) == 2):
+                                _, sent = tmp.split("：")
+                            elif(len(tmp.split("：")) == 1):
+                                sent = tmp.split("：")[3:]
+                            self.sentences.append(sent)
+                        elif(tmp[0]!='＠'):
+                            self.sentences[i-1] += tmp
+                endl+=1
+                return self.sentences
